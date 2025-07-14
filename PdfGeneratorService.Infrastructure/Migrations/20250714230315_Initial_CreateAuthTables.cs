68875568661e2895d83e69059705cc72cd01377c
randradee <dev.renatopandrade@gmail.com>
2025-07-14 20:04:29 -0300

    feat: criação da migration para a tabela de usuários

---
+++ PdfGeneratorService.Infrastructure/Migrations/20250714230315_Initial_CreateAuthTables.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PdfGeneratorService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial_CreateAuthTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "usuarios",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    username = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    password_hash = table.Column<string>(type: "text", nullable: false),
+                    password_salt = table.Column<string>(type: "text", nullable: false),
+                    criado_em = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    atualizado_em = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    ativo = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_usuarios", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "usuarios");
+        }
+    }
+}
